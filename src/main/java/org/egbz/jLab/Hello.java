002cc7bca710ca1252e6fbeec205469d1e0ff4e9
egbz <1023271893@qq.com>
2020-12-14 00:07:54 +0800

    init

---
+++ src/main/java/org/egbz/jLab/Hello.java
@@ -0,0 +1,11 @@
+package org.egbz.jLab;
+
+/**
+ * @author egbz
+ * @date 2020/12/13
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
