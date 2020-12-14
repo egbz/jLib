cf073c3202b4c2ce11c66bbab2145d99f202259f
egbz <1023271893@qq.com>
2020-12-14 23:32:52 +0800

    LockSupportDemo

--- src/main/java/org/egbz/jLab/Hello.java
+++ src/main/java/org/egbz/jLab/Hello.java
@@ -1,11 +1,23 @@
 package org.egbz.jLab;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author egbz
  * @date 2020/12/13
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Integer[] a = {1, 2, 2, 3, 3, 3, 4};
+        for (int ele : a) {
+            System.out.println(ele);
+        }
+
+        ArrayList<Integer> aList = new ArrayList<Integer>(Arrays.asList(a));
+
+        System.out.println("hello world");
     }
 }
