f0cb2dd2a0ccfea42a0637fe1b840e27a23ebc82
egbz <1023271893@qq.com>
2020-12-16 14:53:49 +0800

    CountDownLatch demo (2 CountDownLatch: startSignal, downSignal)

---
+++ src/main/java/org/egbz/jLab/concurrent/countDownLatchDemo/Driver.java
@@ -0,0 +1,29 @@
+package org.egbz.jLab.concurrent.countDownLatchDemo;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author egbz
+ * @date 2020/12/16
+ */
+public class Driver { // ...
+    final int N = 100;
+    void main() throws InterruptedException {
+        CountDownLatch startSignal = new CountDownLatch(1);
+        CountDownLatch doneSignal = new CountDownLatch(N);
+
+        for (int i = 0; i < N; ++i) // create and start threads
+            new Thread(new Worker(startSignal, doneSignal)).start();
+
+        // 这边插入一些代码, 确保上面的每个线程先启动起来, 才执行下面的代码.
+        doSomethingElse();          // don't let run yet
+        // 因为startSignal的初始化参数是1, 所以这里只要调用一次, 那么所有的 await 方法都可以通过
+        startSignal.countDown();      //let all thread proceed
+        doSomethingElse();
+        // 等待所有任务结束
+        doneSignal.await();          // wait for all to finish
+    }
+
+    private void doSomethingElse() {
+    }
+}
---
+++ src/main/java/org/egbz/jLab/concurrent/countDownLatchDemo/Worker.java
@@ -0,0 +1,33 @@
+package org.egbz.jLab.concurrent.countDownLatchDemo;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author egbz
+ * @date 2020/12/16
+ */
+public class Worker implements Runnable {
+    private final CountDownLatch startSignal;
+    private final CountDownLatch doneSignal;
+
+    public Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+        this.startSignal = startSignal;
+        this.doneSignal = doneSignal;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 为了让所有线程同时开始任务, 我们让所有线程先阻塞在这里
+            // 等大家都准备好了, 再打开这个门栓
+            startSignal.await();
+            doWork();
+            doneSignal.countDown();
+        } catch (InterruptedException ex) {
+        } // return
+    }
+
+    private void doWork() {
+        // ...
+    }
+}
