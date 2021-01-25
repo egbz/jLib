4282636be8fadf790faf26e157b89d863a73051d
egbz <1023271893@qq.com>
2021-01-25 23:37:14 +0800

    amend

---
+++ src/main/java/org/egbz/jLab/daily/stream/ParallelStream.java
@@ -0,0 +1,13 @@
+package org.egbz.jLab.daily.stream;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author egbz
+ * @date 2021/1/25
+ */
+public class ParallelStream {
+    public static void main(String[] args) {
+
+    }
+}
