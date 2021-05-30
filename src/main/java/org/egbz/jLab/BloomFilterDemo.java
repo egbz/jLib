ebd06846dd840094e849047db21b5f4dd479955e
egbz <1023271893@qq.com>
2021-05-30 16:39:38 +0800

    amend

---
+++ src/main/java/org/egbz/jLab/BloomFilterDemo.java
@@ -0,0 +1,30 @@
+package org.egbz.jLab;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+/**
+ * @author egbz
+ * @date 2021/5/30
+ */
+public class BloomFilterDemo {
+    public static void main(String[] args) {
+        int total = 1000000;
+        BloomFilter<CharSequence> bf = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), total);
+
+        // 初始化1000000条数据到过滤器中
+        for (int i = 0; i < total; i++) {
+            bf.put("" + i);
+        }
+        // 判断值是否存在过滤器中
+        int count = 0;
+        for (int i = 0; i < total + 10000; i++) {
+            if (bf.mightContain("" + i)) {
+                count++;
+            }
+        }
+
+        System.out.println("matched: " + count);
+    }
+}
