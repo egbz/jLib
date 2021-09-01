3218d8bc423c7e24c741fbd59e7dbef7dd2b3ab2
egbz <1023271893@qq.com>
2021-09-01 23:37:42 +0800

    amend

---
+++ src/main/java/org/egbz/jLab/basis/GenericTest.java
@@ -0,0 +1,44 @@
+package org.egbz.jLab.basis;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author egbz
+ * @date 2021/8/13
+ */
+public class GenericTest {
+
+    static void test1() throws Exception{
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+
+//        list.add("aa");
+
+        // java 伪泛型, 反射可以绕过
+        Class<? extends List> clazz = list.getClass();
+        Method add = clazz.getDeclaredMethod("add", Object.class);
+        add.invoke(list, "aa");
+
+        System.out.println(list);
+    }
+
+    static void test2() {
+        List arrayList = new ArrayList();
+        arrayList.add("aaaa");
+        arrayList.add(100);
+
+        for(int i = 0; i< arrayList.size(); i++){
+            String item = (String)arrayList.get(i);
+
+            // class java.lang.Integer cannot be cast to class java.lang.String
+            System.out.println("泛型测试，item = " + item);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        test1();
+    }
+}
